3c8369ba0024eeeb8e677ccca5ff08ae7ed148cc
Andres Antelis <bantelis10@gmail.com>
2025-11-09 22:11:56 +0100

    Divisoin implementation

--- calculator.py
+++ calculator.py
@@ -5,14 +5,24 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
 a = float(input("First number: "))
-op = input("Operator (+ or -): ")
+op = input("Operator (+, -, *, /-): ")
 b = float(input("Second number: "))
 
 if op == '+':
     print(a + b)
 elif op == '-':
     print(a - b)
+elif op == '/':
+    try:
+        print(a / b)
+    except ValueError as e:
+        print(e)
 else:
     print("Invalid operator!")
 
--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import pytest
-from calculator import add, subtract
+from calculator import add, subtract, divide
 
 def test_add_positive_numbers():
     assert add(2, 3) == 5
@@ -16,3 +16,16 @@ def test_subtract_result_negative():
 def test_add_and_subtract_zero():
     assert add(0, 0) == 0
     assert subtract(0, 0) == 0
+
+def test_divide_positive_numbers():
+    assert divide(10, 2) == 5
+
+def test_divide_negative_numbers():
+    assert divide(-9, 3) == -3
+
+def test_divide_two_negatives():
+    assert divide(-8, -2) == 4
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(5, 0)
